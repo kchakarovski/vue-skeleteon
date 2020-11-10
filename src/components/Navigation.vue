9d93a23846bf81b57e2f0a74e6b5dad5f61a996b
kchakarovski <cakarovskidev@gmail.com>
2020-11-10 18:27:13 +0100

    VS-2 //working

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,51 @@
 <template>
-  <div id="nav">
-      <h1>Nav COMP</h1>
+  <div class="nav">
+      <div class="nav__item" v-for="(value, key) in inputData" :key="key">
+        <slot name="name">
+          <span class="nav__item--name" v-text="value.name" />
+        </slot>
+        <slot name="link">
+          <router-link :to="value.link"><span class="nav__item--link" v-text="value.link"></span></router-link>
+        </slot>
+        <slot name="tooltip">
+          <span class="nav__item--tooltip" v-text="value.tooltip" />
+        </slot>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Navigation",
-    data(){
-        return{
-
-        }
-    },
-}
+  name: "Navigation",
+  props: {
+      inputData: {
+          type: Array,
+          default: () => {
+              return [
+                {
+                  name: 'Lorem Ipsum',
+                  link: 'https://google.com',
+                  tooltip: 'Some Tooltip'
+              }
+                // {
+                //   name: 'Lorem Ipsum',
+                //   link: 'https://google.com',   
+                //   tooltip: 'Some Tooltip'
+                // },
+                // {
+                //   name: 'Lorem Ipsum',
+                //   link: 'https://google.com',   
+                //   tooltip: 'Some Tooltip'
+                // }
+              ];
+          },
+      },
+  },
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-
 </style>
